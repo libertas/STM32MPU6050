89d192e893357b3e171e69b2988ef4bf6d3a5614
libertas <horizonvei@gmail.com>
2015-11-24 21:18:43 +0800

    can now get accel successfully

--- Project/kinestate.c
+++ Project/kinestate.c
@@ -9,15 +9,9 @@ struct kine_state get_kine_state(struct kine_state *now_state)
 	ay = mpu6050_get_exact_data(ACCEL_YOUT_H);
 	az = mpu6050_get_exact_data(ACCEL_ZOUT_H);
 
-	/*
 	result.ax = ax * ACCEL_RANGE / 32767;
 	result.ay = ay * ACCEL_RANGE / 32767;
 	result.az = az * ACCEL_RANGE / 32767;
-	*/
-	
-	result.ax = int2double(ax);
-	result.ay = int2double(ay);
-	result.az = int2double(az);
 
 	return result;
 }
