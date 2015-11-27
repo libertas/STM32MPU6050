f67d9430e1c6c1bd2c9575abb480440cf244eb27
libertas <horizonvei@gmail.com>
2015-11-27 21:02:27 +0800

    test can display data now

--- Project/test/test.py
+++ Project/test/test.py
@@ -3,23 +3,24 @@
 import serial
 
 if __name__ == "__main__":
+    # acquire the data
+    print("Acquiring data...")
+    
     data_buf = []
     wrong_format_time = 0
-    
+
     range_max = 1000
-    
+
     serial_port = "/dev/ttyUSB0"
     serial_baud = 115200
     se = serial.Serial(serial_port, serial_baud)
-    
+
     for i in range(0,  range_max):
         try:
             str_buf = se.readline().decode("ascii")
         except UnicodeDecodeError:
             continue
-            
-        # print("Received data:%s" % str_buf)
-        
+
         try:
             list_buf = str_buf.split(",")
             if len(list_buf) == 3:
@@ -42,5 +43,30 @@ if __name__ == "__main__":
                     exit(-1)
         except:
             exit(-2)
+
+    se.close()
+
+    # plot
+    import numpy as np
+    import matplotlib.pyplot as plt
     
-    print(data_buf)
+    ax_list = []
+    ay_list = []
+    az_list =[]
+    
+    for i in range(0,  len(data_buf)):
+        tmp = data_buf[i]
+        ax_list.append(tmp[0])
+        ay_list.append(tmp[1])
+        az_list.append(tmp[2])
+    
+    x = np.arange(0,  len(data_buf),  1)
+    ax = np.array(ax_list)
+    ay = np.array(ay_list)
+    az = np.array(az_list)
+    
+    plt.plot(x,  ax,  label = "$ax$",  color = "red")
+    plt.plot(x,  ay,  label = "$ay$",  color = "blue")
+    plt.plot(x,  az,  label = "$az$",  color = "black")
+    plt.legend()
+    plt.show()
